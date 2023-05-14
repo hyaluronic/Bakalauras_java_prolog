742c04a607385186844625c635bf2c5087295aaf
hyaluronic <60736298+hyaluronic@users.noreply.github.com>
2023-05-14 19:53:04 +0300

    improvement to UX

--- src/main/java/com/pltl/prolog/controller/TheoremController.java
+++ src/main/java/com/pltl/prolog/controller/TheoremController.java
@@ -1,7 +1,7 @@
 package com.pltl.prolog.controller;
 
 import com.pltl.prolog.model.TreeNode;
-import com.pltl.prolog.service.PrologQuery;
+import com.pltl.prolog.service.PrologService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,14 +11,14 @@ import org.springframework.web.bind.annotation.*;
 public class TheoremController {
 
     @Autowired
-    private PrologQuery prologQuery;
+    private PrologService prologService;
     @GetMapping(value = "/")
     public String defaultTheorem(Model model) {
         return "index";
     }
+
     @PostMapping
     public TreeNode validateTheorem(@RequestBody String theorem) {
-        //TODO: parser to acceptable query?
 //        theorem = "[c, g, neg p, a, p, d] => [s]";
 //        theorem = "[c and g, neg s, a and b,  p, d] => [b and p and a, b, a]";
 //        theorem = "[a, b] => [a and b,c,d,e,r]";
@@ -26,6 +26,6 @@ public class TheoremController {
 //        theorem = "[c or (a or b)] => [a,b,c]";
 //        theorem = "[(c or b) or (a or b)] => [a,b,c]";
 //        theorem = "[((c or a) or (a or b)) or (a or b)] => [a,b,c]";
-        return prologQuery.queryProve(theorem);
+        return prologService.queryProve(theorem);
     }
 }
